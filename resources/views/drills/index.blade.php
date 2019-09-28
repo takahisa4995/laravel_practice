c4e9eec4b6aa482be9469cd4a1213069a518a70d
たかひさ <takaku.genki021@gmail.com>
2019-09-28 11:58:42 +0900

    バグ修正

--- resources/views/drills/index.blade.php
+++ resources/views/drills/index.blade.php
@@ -11,7 +11,8 @@
         <div class="card">
           <div class="card-body">
             <h3 class="card-title">{{ $drill->title }}</h3>
-            <a href="#" class="btn btn-primary">{{ __('Go Practice') }}</a>
+          <a href="#" class="btn btn-primary">{{ __('Go Practice') }}</a>
+          <a href="{{ route('drills.edit',$drill->id) }}" class="btn btn-warning">{{ __('Go Edit') }}</a>
           </div>
         </div>
       </div>
