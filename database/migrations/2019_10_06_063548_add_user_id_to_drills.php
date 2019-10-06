4a00681472032a483f05a68c423725914b2d844b
たかひさ <takaku.genki021@gmail.com>
2019-10-06 16:31:36 +0900

    Drillsテーブルにuser_idを追加

---
+++ database/migrations/2019_10_06_063548_add_user_id_to_drills.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToDrills extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('drills', function (Blueprint $table) {
+            Schema::table('drills', function (Blueprint $table) {
+                DB::statement('DELETE FROM drills');
+                $table->unsignedBigInteger('user_id');
+                $table->foreign('user_id')->references('id')->on('users');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('drills', function (Blueprint $table) {
+            Schema::table('drills', function (Blueprint $table) {
+                $table->dropForeign(['user_id']);
+                $table->dropColumn('user_id');
+            });
+        });
+    }
+}
